38bc60fa5ea38f758cc012c301f5d7cd915582df
SolMon9099 <101564382+SolMon9099@users.noreply.github.com>
2023-10-01 15:36:03 -0400

    📢 Events (#785)
    * Event Features
    
    * Event Feature
    
    * Restore db setting

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -94,6 +94,7 @@ def create_app():
         admin_notifications,
         masters,
         translation,
+        events
     )
 
     # why blueprints http://flask.pocoo.org/docs/1.0/blueprints/
@@ -115,6 +116,8 @@ def create_app():
     app.register_blueprint(notifications.notifications, url_prefix="/api/notifications")
     app.register_blueprint(masters.masters, url_prefix="/api/masters")
     app.register_blueprint(translation.translation, url_prefix="/api/translation")
+
+    app.register_blueprint(events.event, url_prefix="/api")
 
     app.register_error_handler(Exception, all_exception_handler)
 
